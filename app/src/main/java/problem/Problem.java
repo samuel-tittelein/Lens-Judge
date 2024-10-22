15159664507c4ce5ffc40b4defc99b2d59c25c21
romain.lechene <romain_lechene@ens.univ-artois.fr>
2024-10-22 14:50:59 +0200

    Added javadoc

--- app/src/main/java/problem/Problem.java
+++ app/src/main/java/problem/Problem.java
@@ -12,6 +12,9 @@ public class Problem implements Iterable<TestCase>{
     private final int memoryLimit;
     private final IVerifier verifier;
 
+    /**
+     * Creates an instance of Problem using the builder and its attributes
+     */
     public Problem(ProblemBuilder b){
         this.testCasesList = b.getTestCasesList();
         this.timeLimit = b.getTimeLimit();
@@ -19,32 +22,60 @@ public class Problem implements Iterable<TestCase>{
         this.verifier = b.getVerifier();
     }
 
+    /**
+     *
+     * @return the list of test cases from the Problem
+     */
     public List<TestCase> getTestCasesList() {
         return testCasesList;
     }
 
+    /**
+     *
+     * @return the time limit of the problem's execution in seconds
+     */
     public int getTimeLimit() {
         return timeLimit;
     }
 
+    /**
+     *
+     * @return the memory limit of the problem in bytes
+     */
     public int getMemoryLimit() {
         return memoryLimit;
     }
 
+    /**
+     *
+     * @return the problem's verifier
+     */
     public IVerifier getVerifier() {
         return verifier;
     }
 
+    /**
+     *
+     * @return the iterator associated with the list of test cases
+     */
     @Override
     public Iterator<TestCase> iterator() {
         return testCasesList.iterator();
     }
 
+    /**
+     *
+     * Implementation of the method forEach of Iterable
+     */
     @Override
     public void forEach(Consumer<? super TestCase> action) {
         Iterable.super.forEach(action);
     }
 
+    /**
+     *
+     * @return the problem as spliterator
+     */
     @Override
     public Spliterator<TestCase> spliterator() {
         return Iterable.super.spliterator();
