f2e53f2a2ba91911549ab79053398e81744d1033
samuel_tittelein <samuel_tittelein@ens.univ-artois.fr>
2024-10-22 10:46:06 +0200

    :zap: update compiler interface

--- app/src/main/java/compiler/ICompiler.java
+++ app/src/main/java/compiler/ICompiler.java
@@ -1,5 +1,26 @@
 package compiler;
 
-public interface ICompiler {
+import java.io.File;
 
+public interface ICompiler {
+    /**
+     * check the compatibility of the source file extension
+     * @param sourceFile The source file
+     * @return boolean indicating if the extention is supported
+     */
+    boolean isCompatible(File sourceFile);
+
+    /**
+     * Deduce from the name of the source file the name of the compiled binary.
+     * @param sourceFile
+     * @return String the name of the binary file
+     */
+    String binName(File sourceFile);
+
+    /**
+     * run the compiler command
+     * @param sourceFile
+     * @return
+     */
+    File compile(File sourceFile);
 }
