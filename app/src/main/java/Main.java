be5f71fee8d2bc908d43752d57689c9cf71ddecd
samuel_tittelein <samuel_tittelein@ens.univ-artois.fr>
2024-10-23 14:47:56 +0200

    :sparkles: #8 add Main.java

---
+++ app/src/main/java/Main.java
@@ -0,0 +1,19 @@
+import compiler.ICompiler;
+import executer.IExecuter;
+import problem.Problem;
+
+public class Main {
+    private ICompiler compiler;
+    private IExecuter executer;
+    private Problem problem;
+
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Enter the file name to test");
+            System.out.println("Enter the directory where ");
+        }
+
+    }
+
+}
