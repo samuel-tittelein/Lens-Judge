b7b385c6182096adfadfdfa6fd8d9193c3b5e7e5
samuel_tittelein <samuel_tittelein@ens.univ-artois.fr>
2024-10-23 11:09:11 +0200

    #5 implementation of PythonCompiler

--- app/src/main/java/compiler/PythonCompiler.java
+++ app/src/main/java/compiler/PythonCompiler.java
@@ -1,10 +1,19 @@
 package compiler;
 
 import java.io.File;
+import java.util.List;
 
 public class PythonCompiler extends AbstractCompiler implements ICompiler{
     @Override
     public File compile(File sourceFile) throws IllegalArgumentException {
+        try {
+            processController.startProcess(
+                    List.of("python3", "-m", "py_compile", sourceFile.getAbsolutePath())
+            );
+            processController.waitForCompletion();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
         return sourceFile;
     }
 }
