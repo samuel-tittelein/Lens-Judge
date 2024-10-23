49e846608981d73690a05622ad793fd0605d47b6
Fierquin Matteo <matteo_fierquin@ens.univ-artois.fr>
2024-10-23 12:19:59 +0200

    Created a c and c++ executer and a test for it #6

--- app/src/main/java/executer/CExecuter.java
+++ app/src/main/java/executer/CExecuter.java
@@ -4,18 +4,25 @@ import process.IProcess;
 import process.ProcessController;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
 
-public class CExecuter implements IExecuter {private IProcess process;
+public class CExecuter implements IExecuter {
+    private IProcess process;
 
     public CExecuter() {
         process = new ProcessController();
     }
 
     @Override
-    public void execute(File file) throws IOException, InterruptedException {
+    public void execute(File file, File input) throws IOException, InterruptedException {
         process.startProcess(List.of(file.getAbsolutePath()));
+        if (input != null && input.exists()) {
+            try (FileInputStream fis = new FileInputStream(input)) {
+                process.provideInput(fis);
+            }
+        }
         process.waitForCompletion();
         process.writeStandardOutputToFile(file.getName() + ".out");
     }
