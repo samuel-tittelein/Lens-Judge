cda9fe164ccced5d8a2d9e8c47ad1e670e936ca5
Fierquin Matteo <matteo_fierquin@ens.univ-artois.fr>
2024-10-23 12:16:52 +0200

    Created an interface for executers and a proxy to choose which one to use #6

--- app/src/main/java/executer/ExecuterProxy.java
+++ app/src/main/java/executer/ExecuterProxy.java
@@ -9,17 +9,20 @@ public class ExecuterProxy implements IExecuter {
     private IExecuter executer;
 
     @Override
-    public void execute(File file) throws IOException, InterruptedException {
+    public void execute(File file, File input) throws IOException, InterruptedException {
         if (file == null)
             throw new RuntimeException("The file is null.");
+        try{
         String extension = AbstractCompiler.getExtension(file);
         switch (extension) {
             case "class" -> executer = new JavaExecuter();
-            case "" -> executer = new CExecuter();
             case "py" -> executer = new PythonExecuter();
             default -> throw new IllegalArgumentException("The source file has an unsupported extension.");
+        }}
+        catch (IllegalArgumentException e){
+            executer = new CExecuter();
         }
-        executer.execute(file);
+        executer.execute(file, input);
     }
 
 
--- app/src/main/java/executer/IExecuter.java
+++ app/src/main/java/executer/IExecuter.java
@@ -8,8 +8,9 @@ interface IExecuter {
     /**
      * Put the output of the execution in a file name {filename}.out
      * @param file A python, java or c file compiled
+     * @param input The input file to pass to the script (can be null).
      * @throws IOException
      * @throws InterruptedException
      */
-    void execute(File file) throws IOException, InterruptedException;
+    void execute(File file, File input) throws IOException, InterruptedException;
 }
