626bed4d5cf5aa6f90728fd32e40bafbe44e32ea
samuel_tittelein <samuel_tittelein@ens.univ-artois.fr>
2024-10-23 13:42:10 +0200

    :memo: #5 add javadoc

--- app/src/main/java/compiler/JavaCompiler.java
+++ app/src/main/java/compiler/JavaCompiler.java
@@ -1,15 +1,29 @@
 package compiler;
 
+import process.ProcessController;
+
 import java.io.File;
 import java.util.List;
 
+/**
+ * implements a java compiler
+ * @author samuel.tittelein
+ */
 public class JavaCompiler extends AbstractCompiler implements ICompiler {
+
+    /**
+     * compile the given file with the command : "javac -c [sourceDirectory] [sourceFileName]"
+     * @param sourceFile The source file
+     * @return the compiled file
+     * @throws IllegalArgumentException
+     */
     @Override
     public File compile(File sourceFile) throws IllegalArgumentException {
 
         String sourceDirectory = sourceFile.getParentFile().getAbsolutePath();
         List<String> cmd = List.of("javac", "-d", sourceDirectory, sourceFile.getName());
 
+        processController = new ProcessController();
         try {
             processController.startProcess(cmd);
             processController.waitForCompletion();
